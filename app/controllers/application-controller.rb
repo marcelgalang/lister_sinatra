5e5e12bb3e33eaaa9efbc4e52b2f3c2652d436af
Marcel Galang <marcelgalang@gmail.com>
2016-09-18 02:42:51 +0000

    created new branch and starter files

---
+++ app/controllers/application-controller.rb
@@ -0,0 +1,11 @@
+require "./config/environment"
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :views, "app/views"
+    enable :sessions
+    set :session_secret, "password_security"
+  end
+
+end
