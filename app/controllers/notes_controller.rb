3e06c05357be10a0c03a09e8a040e42b0a7d0acf
Marcel Galang <marcelgalang@gmail.com>
2016-09-18 15:51:46 +0000

    Add routes to notes and user controllers

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -28,7 +28,56 @@ class NotesController < ApplicationController
     end
   end
 
+  get '/notes/:id' do
+    if logged_in?
+      @note = Note.find_by_id(params[:id])
+      erb :'/notes/show'
+    else
+      redirect to '/login'
+    end
   end
+
+  post '/notes/:id/edit' do
+    if session[:user_id]
+      @note = Note.find_by_id(params[:id])
+      #give permissions to other users here?
+      if @note.user_id == session[:user_id]
+        erb :'notes/edit'
+      else
+        redirect to '/notes'
+      end
+    else
+      redirect to '/login'
+    end
+  end
+
+  patch '/notes/:id' do
+    if params[content]==""
+      redirect to '/notes/#{params[:id]}/edit'
+    else
+      @note = Note.find_by_id(params[:id])
+      @note.content = params[:content]
+      @note.save
+      redirect to '/notes/#{notes.id}'
+    end
+  end
+
+  delete '/notes/:id/delete' do
+    @note = Note.find_by_id(params[:id])
+
+    if logged_in?
+      if @note.user_id == session[:user_id]
+        @note.delete
+        redirect to '/notes'
+      else
+        redirect to '/notes'
+      end
+    else
+        redirect to '/login'
+    end
+  end
+
+
 
 
 end
